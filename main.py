6c941fd0869f5ce7c7dc12225112d53dad92cc3c
hugo.chang <hmchang0904@gmai.com>
2021-01-11 11:02:40 +0800

    test

--- main.py
+++ main.py
@@ -7,25 +7,33 @@ import a2_list
 
 
 def print_title():
+    print("")
+    print("-"*30)
     print("1. String Operation")
     print("2. List Operation")
     print("3. Game Operation")
     print("4. Math Operation")
+    print("5. Exit")
 
 
 if __name__ == '__main__':
 
-    print_title()
-    select_value = int(input("Please select your operation:"))
 
-    if select_value == 1:
-        a1_string.string_operation(input("What is your testing string: "))
-    elif select_value == 2:
-        a2_list.list_operation()
-    elif select_value == 3:
-        game.game_start()
-    elif select_value == 4:
-        math_cal.print_mathtable()
-        math_cal.Guess_Number(50)
-    else:
-        print("Your operation nnot support. Bye!!")
+    select_value = 0
+
+    while (select_value != 5):
+        print_title()
+        select_value = int(input("Please select your operation:"))
+
+        if select_value == 1:
+            a1_string.string_operation(input("What is your testing string: "))
+        elif select_value == 2:
+            a2_list.list_operation()
+        elif select_value == 3:
+            game.game_start()
+        elif select_value == 4:
+            math_cal.print_mathtable()
+            math_cal.Guess_Number(50)
+        
+    print("Bye! Bye!")
+    
